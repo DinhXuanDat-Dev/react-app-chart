08cb2194576080bcc94d5a9172635851e3961567
DatDX <dinh_xuan_dat@allgrow-labo.jp>
2022-03-16 15:09:56 +0700

    component Cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,6 +3,10 @@ import React from "react"
 import { Grid, Card, CardContent, Typography, } from "@material-ui/core"
 import CountUp from "react-countup";
 
+import cx from 'classnames';
+
+import styles from "./Cards.module.css"
+
 // const Cards = ({ data : {confirmed , recovered, deaths, lastUpdated} }) => {
 const Cards = ( data ) => {
 
@@ -11,9 +15,12 @@ const Cards = ( data ) => {
     const { confirmed, recovered, deaths, lastUpdate} = data.data
 
     return (
-        <Grid container spacing={3} justifyContent="center" >
-            <Grid item component={Card}>
+        <Grid wrap='no-wrap' container spacing={3} justifyContent="center" className={styles.container}>
+            <Grid item component={Card} xs={12} md={3} className={ cx(styles.card, styles.infected)}>
                 <CardContent>
+                    <Typography gutterBottom variant="h4" >
+                        Infected
+                    </Typography>
                     <Typography gutterBottom variant="h5" component="div">
                         <CountUp 
                             start = {0}
@@ -21,52 +28,44 @@ const Cards = ( data ) => {
                             duration = {2.5}
                             separator = "."
                         />
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                        {recovered.value}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                        {deaths.value}
                     </Typography>
                     <Typography variant="body2" color="textSecondary">
                         {new Date(lastUpdate).toDateString()}
                     </Typography>
                 </CardContent>
+            </Grid>
 
+            <Grid item component={Card} xs={12} md={3} className={ cx(styles.card, styles.recover)}>
                 <CardContent>
+                    <Typography gutterBottom variant="h4" component="div">
+                        Recover
+                    </Typography>
                     <Typography gutterBottom variant="h5" component="div">
                         <CountUp 
                             start = {0}
-                            end = {confirmed.value}
+                            end = {recovered.value}
                             duration = {2.5}
                             separator = "."
                         />
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                        {recovered.value}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                        {deaths.value}
                     </Typography>
                     <Typography variant="body2" color="textSecondary">
                         {new Date(lastUpdate).toDateString()}
                     </Typography>
                 </CardContent>
+            </Grid>
 
+            <Grid item component={Card} xs={12} md={3} className={ cx(styles.card, styles.deaths)}>
                 <CardContent>
+                    <Typography gutterBottom variant="h4" component="div">
+                        Deaths
+                    </Typography>
                     <Typography gutterBottom variant="h5" component="div">
                         <CountUp 
                             start = {0}
-                            end = {confirmed.value}
+                            end = {deaths.value}
                             duration = {2.5}
                             separator = "."
                         />
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                        {recovered.value}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                        {deaths.value}
                     </Typography>
                     <Typography variant="body2" color="textSecondary">
                         {new Date(lastUpdate).toDateString()}
